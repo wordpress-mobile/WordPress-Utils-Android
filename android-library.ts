b8c1da973a41005b6b12d843bc200f1bc244cf51
Gio Lodi <giovanni.lodi42@gmail.com>
2020-11-18 22:16:51 +1100

    Update android-library.ts
    See https://github.com/Automattic/peril-settings/pull/58/commits/1cefb3de636f67d836a66107c32f253b7b93bacd

--- android-library.ts
+++ android-library.ts
@@ -1,28 +1,30 @@
 import {fail, danger} from "danger";
 
+// We want each PR towards develop or trunk of our Android libraries to bump
+// the library version. This check makes sure of it.
 export default async () => {
-  if (danger.github.pr.base.ref != "develop") { return }
+  const baseBranch = danger.github.pr.base.ref
+  if (baseBranch != "develop" && baseBranch != "trunk") { return }
 
-  const modified_build_gradles = danger.git.modified_files.filter((path: string) => path.endsWith('build.gradle'))
+  // Android projects have more than one build.gradle file, to be able to run
+  // this check on any library, lets's just look into _all_ the build.gradle
+  // files.
+  const modified = danger.git.modified_files
+    .filter((path: string) => path.endsWith('build.gradle'))
 
-  if (modified_build_gradles.length == 0) {
-    fail("Please update the library version before merging into `develop`")
-    return
-  }
-
-  // TODO: need to support multiple files
-  var matchFound = false
-  for (let file of modified_build_gradles) {
+  for (let file of modified) {
     const diff = await danger.git.diffForFile(file)
 
     // We could be more refiened and actually pares the .before and .after
     // contents to extract the versionName value and do a SemVer check on it to
-    // make sure it's appropriate
-    matchFound = diff.after.includes('versionName "')
-    if (matchFound) { break }
+    // make sure it's appropriate.
+    //
+    // The '"' is a way to make user the actual versionName definition change,
+    // not a line using versionName.
+    const matchFound = diff.after.includes('versionName "')
+    if (matchFound) { return }
   }
 
-  if (matchFound == false) {
-    fail("Please update the library version before merging into `develop`")
-  }
+  // If we're here, no match was found: fail the build.
+  fail(`Please update the library version before merging into \`${baseBranch}\``)
 }
